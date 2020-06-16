9e6f6d849149dd81138042d319f33f0791416028
Hailee Doyle <haileedoyle@Hailees-MacBook-Pro.local>
2020-06-16 09:48:15 -0500

    mongoose installed, server setup

--- server.js
+++ server.js
@@ -1,1 +1,44 @@
-console.log('Hello World');
+// console.log('Hello World');
+//======================
+// dependencies
+//======================
+
+const express = require('express')
+const app = express()
+
+// mongoose
+const mongoose = require('mongoose')
+
+//======================
+// use
+//======================
+
+app.use(express.json())
+app.use(express.static('public'))
+
+//======================
+// middleware
+//======================
+
+
+
+// use
+
+
+//======================
+// mongoose connect
+//======================
+
+mongoose.connect('mongodb://localhost:27017/helloworld',
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+  mongoose.connection.once('open', () => {
+      console.log('Mongod connection established.');
+  });
+
+//======================
+// listener
+//======================
+
+app.listen(3000, () => {
+  console.log('Started listening.');
+})
