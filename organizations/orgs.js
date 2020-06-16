e2596a65cb8845b595921a6a7c38a83acf0cb342
Zach Herwitz <zach.herwitz@gmail.com>
2020-06-16 17:09:01 -0400

    adding first block of orgs

---
+++ organizations/orgs.js
@@ -0,0 +1,124 @@
+// ORGANIZATIONS FROM https://docs.google.com/spreadsheets/d/1Pf76L5azQN9ahbhbt7MRmKb5sBZgeUWbXKLoPJO592Y/edit#gid=0
+
+// SCHEMA:
+// {
+//   organization: String,
+//   website: String,
+//   donationMethod: String,
+//   bipocFounded: Boolean,
+//   lgbtqiaFounded: Boolean,
+//   missionStatement: String,
+//   donated: Boolean,
+//   donatedAmount: Number
+// }
+
+{
+  organization: 'Black Lives Matter',
+  website: 'https://blacklivesmatter.com/',
+  donationMethod: 'via ActBlue',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'Black Lives Matter Foundation, Inc is a global organization in the US, UK, and Canada, whose mission is to eradicate white supremacy and build local power to intervene in violence inflicted on Black communities by the state and vigilantes.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'BlackOUT Collective',
+  website: 'https://blackoutcollective.org/',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'We build organizations’ capacity to execute creative and effective direct actions in service of their organizing and advocacy work. We do this through providing personalized direct action trainings and on the ground action support.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Campaign Zero',
+  website: 'https://www.joincampaignzero.org/',
+  donationMethod: 'Paypal',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'Funds donated to Campaign Zero support the analysis of policing practices across the country, research to identify effective solutions to end police violence, technical assistance to organizers leading police accountability campaigns and the development of model legislation and advocacy to end police violence nationwide. ',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Color of Change',
+  website: 'https://colorofchange.org/',
+  donationMethod: 'via ActBlue',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'Move decision-makers in corporations and government to create a more human and less hostile world for Black people in America',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Movement for Black Lives',
+  website: 'https://m4bl.org/',
+  donationMethod: 'via ActBlue',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'The Movement for Black Lives is an ecosystem of individuals and organizations creating a shared vision and policy agenda to win rights, recognition, and resources for Black people.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'NAACP',
+  website: 'https://www.naacp.org/',
+  donationMethod: 'via ActBlue',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'The mission of the National Association for the Advancement of Colored People (NAACP) is to secure the political, educational, social, and economic equality of rights in order to eliminate race-based discrimination and ensure the health and well-being of all persons.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'National Police Accountability Project (National Lawyers Guild)',
+  website: 'https://www.nlg-npap.org/donate/',
+  donationMethod: 'PayPal',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'Promote the accountability of law enforcement officers and their employers for violations of the Constitution and the laws of the United States.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Reclaim the Block',
+  website: 'https://www.reclaimtheblock.org/home',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: "Thank you for supporting Reclaim the Block's work to defund police in Minneapolis and move our community's dollars into resources that actually keep us safe.",
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Showing Up for Racial Justice (SURJ)',
+  website: 'https://www.showingupforracialjustice.org/justiceforahmaud.html',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'SURJ is a national network of groups and individuals working to undermine white supremacy and to work toward racial justice. Through community organizing, mobilizing, and education, SURJ moves white people to act as part of a multi-racial majority for justice with passion and accountability.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Southern Poverty Law Center Action Fund',
+  website: 'https://www.splcenter.org/what-splc-action-fund',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'Flexibility to engage in legislative battles at every level of government and to support critical ballot initiatives',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: "Stand with Minneapolis Campaign (Al-Maa'uun Organization)",
+  website: 'https://www.launchgood.com/campaign/stand_with_minneapolis_we_cant_breathe#!/',
+  donationMethod: 'Crowd Funding',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: "Al-Maa'uun is a local, Black-led, grass-roots organization that serves thousands of households each year, with a focus on north Minneapolis. Your generous donations will provide Food, Basic household supplies, & urgently needed medications to our affected neighborhoods right now.",
+  donated: false,
+  donatedAmount: 0
+},
