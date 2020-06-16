33d6b3f9c76bb09cf5cdb22d1acc4adf95d9c3fa
Hailee Doyle <haileedoyle@Hailees-MacBook-Pro.local>
2020-06-16 17:02:27 -0500

    seed data part 1

---
+++ models/seed.js
@@ -0,0 +1,321 @@
+const seedData =
+{
+  organization: 'Black Lives Matter',
+  website: 'https://blacklivesmatter.com/',
+  donationMethod: 'via ActBlue',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'Black Lives Matter Foundation, Inc is a global organization in the US, UK, and Canada, whose mission is to eradicate white supremacy and build local power to intervene in violence inflicted on Black communities by the state and vigilantes.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'BlackOUT Collective',
+  website: 'https://blackoutcollective.org/',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'We build organizations’ capacity to execute creative and effective direct actions in service of their organizing and advocacy work. We do this through providing personalized direct action trainings and on the ground action support.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Campaign Zero',
+  website: 'https://www.joincampaignzero.org/',
+  donationMethod: 'Paypal',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'Funds donated to Campaign Zero support the analysis of policing practices across the country, research to identify effective solutions to end police violence, technical assistance to organizers leading police accountability campaigns and the development of model legislation and advocacy to end police violence nationwide. ',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Color of Change',
+  website: 'https://colorofchange.org/',
+  donationMethod: 'via ActBlue',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'Move decision-makers in corporations and government to create a more human and less hostile world for Black people in America',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Movement for Black Lives',
+  website: 'https://m4bl.org/',
+  donationMethod: 'via ActBlue',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'The Movement for Black Lives is an ecosystem of individuals and organizations creating a shared vision and policy agenda to win rights, recognition, and resources for Black people.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'NAACP',
+  website: 'https://www.naacp.org/',
+  donationMethod: 'via ActBlue',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'The mission of the National Association for the Advancement of Colored People (NAACP) is to secure the political, educational, social, and economic equality of rights in order to eliminate race-based discrimination and ensure the health and well-being of all persons.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'National Police Accountability Project (National Lawyers Guild)',
+  website: 'https://www.nlg-npap.org/donate/',
+  donationMethod: 'PayPal',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'Promote the accountability of law enforcement officers and their employers for violations of the Constitution and the laws of the United States.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Reclaim the Block',
+  website: 'https://www.reclaimtheblock.org/home',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: "Thank you for supporting Reclaim the Block's work to defund police in Minneapolis and move our community's dollars into resources that actually keep us safe.",
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Showing Up for Racial Justice (SURJ)',
+  website: 'https://www.showingupforracialjustice.org/justiceforahmaud.html',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'SURJ is a national network of groups and individuals working to undermine white supremacy and to work toward racial justice. Through community organizing, mobilizing, and education, SURJ moves white people to act as part of a multi-racial majority for justice with passion and accountability.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Southern Poverty Law Center Action Fund',
+  website: 'https://www.splcenter.org/what-splc-action-fund',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'Flexibility to engage in legislative battles at every level of government and to support critical ballot initiatives',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: "Stand with Minneapolis Campaign (Al-Maa'uun Organization)",
+  website: 'https://www.launchgood.com/campaign/stand_with_minneapolis_we_cant_breathe#!/',
+  donationMethod: 'Crowd Funding',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: "Al-Maa'uun is a local, Black-led, grass-roots organization that serves thousands of households each year, with a focus on north Minneapolis. Your generous donations will provide Food, Basic household supplies, & urgently needed medications to our affected neighborhoods right now.",
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'ACLU',
+  website: 'https://www.aclu.org/',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'The ACLU works in the courts, legislatures and communities to defend and preserve the individual rights and liberties guaranteed to all people in this country by the Constitution and laws of the United States.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Anti Police Terror Project',
+  website: 'https://www.antipoliceterrorproject.org/',
+  donationMethod: 'PayPal',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'The Anti Police-Terror Project is a Black-led, multi-racial, intergenerational coalition that seeks to build a replicable and sustainable model to eradicate police terror in communities of color.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Baltimore Action Legal Team',
+  website: 'https://www.baltimoreactionlegal.org/donate',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'BALT provides legal support to Baltimore communities as they exercise their civil liberties protesting against injustices rooted in structural racism and economic inequality.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'DC Legal Support Fund (Black Lives Matter)',
+  website: 'https://www.gofundme.com/f/defendblmdc',
+  donationMethod: 'Crowd Funding',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'From Copwatch, protest support, to jail care and support, representational support all other related support, it all takes resources. Please donate to ensure we are able to continue to protect and defend Black People in DC.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Detroit Justice Center',
+  website: 'https://www.detroitjustice.org/',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'The Detroit Justice Center (DJC) is a non-profit law firm working alongside communities to create economic opportunities, transform the justice system, and promote equitable and just cities.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Equal Justice Initiative',
+  website: 'eji.org',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'Committed to ending mass incarceration and excessive punishment in the United States, to challenging racial and economic injustice, and to protecting basic human rights for the most vulnerable people in American society.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Good Call',
+  website: 'https://goodcall.nyc',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'Free legal support in NYC boroughs',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Law for Black Lives',
+  website: 'http://www.law4blacklives.org/',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'Law For Black Lives is a Black femme-led national network of radical lawyers and legal workers of color committed to building a responsive legal infrastructure for movement organizations and the Movement for Black Lives.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Legal Aid NYC',
+  website: 'https://legalaidnyc.org/',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'The Legal Aid Society works throughout New York City to ensure everyone has access to justice and to defend the rights of everyday New Yorkers who need legal support.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Legal Defense Initiative (Know Your Rights Camp)',
+  website: 'https://www.knowyourrightscamp.com/legal',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'The Know Your Rights Camp Legal Defense Initiative has identified and teamed up with top defense lawyers and civil rights lawyers nationwide to provide legal resources for those in need of legal assistance, has been arrested or has been a victim of police brutality while fighting injustice across the nation.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Legal Services for Prisoners with Children',
+  website: 'https://prisonerswithchildren.org',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'LSPC organizes communities impacted by the criminal justice system and advocates to release incarcerated people, to restore human and civil rights, and to reunify families and communities. We build public awareness of structural racism in policing, the courts, and the prison system, and we advance racial and gender justice in all our work.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Ohio Justice and Policy Center',
+  website: 'http://www.ohiojpc.org',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'OJPC leads in serving the needs of people directly affected by the criminal justice system collaborating with individuals and organizations to develop public policy that spreads fair, intelligent, redemptive criminal justice through Ohio.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Pure Justice',
+  website: 'https://purejustice.org/',
+  donationMethod: 'PayPal',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'Advocate for the changes in our criminal justice system and for economic opportunities. Aim to transform institutions, create equity, and improve the lives of low to moderate income families in Harris County.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Southern Poverty Law Center',
+  website: 'https://www.splcenter.org/',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'Support research, litigation, advocacy and public education efforts.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'The Center for Constitutional Rights',
+  website: 'https://ccrjustice.org/',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'The Center for Constitutional Rights works with communities under threat to fight for justice and liberation through litigation, advocacy, and strategic communications. Since 1966, we have taken on oppressive systems of power, including structural racism, gender oppression, economic inequity, and governmental overreach.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Thurgood Marshall Institute Legal Defense Fund',
+  website: 'https://tminstituteldf.org/',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: true,
+  lgbtqiaFounded: false,
+  missionStatement: 'The Thurgood Marshall Institute is a multidisciplinary center within the NAACP Legal Defense Fund targeting advocacy campaigns that advance the work of racial justice. ',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Transgender Law Center',
+  website: 'https://transgenderlawcenter.org',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: true,
+  missionStatement: 'Transgender Law Center is the largest national trans-led organization advocating for a world in which all people are free to define themselves and their futures by changing law & policy.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'The Bronx Defenders',
+  website: 'https://www.bronxdefenders.org/',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'The Bronx Defenders is a public defender nonprofit that is radically transforming how low-income people in the Bronx are represented in the justice system and, in doing so, is transforming the system itself.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Brooklyn Defender Services',
+  website: 'http://bds.org/',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'The mission of Brooklyn Defender Services is a pulic defender organization providing high quality legal representation and related services to people who cannot afford to retain an attorney.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Queens Defenders',
+  website: 'https://queensdefenders.org/',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: 'Queens Defenders represents people who have been charged with a crime in Queens and cannot afford to hire a lawyer.',
+  donated: false,
+  donatedAmount: 0
+},
+{
+  organization: 'Neighborhood Defender Service',
+  website: 'https://www.neighborhooddefender.org/',
+  donationMethod: 'Credit/Debit',
+  bipocFounded: false,
+  lgbtqiaFounded: false,
+  missionStatement: '(NDS) is known nationally and internationally for its innovative, community-based, holistic public defense practice.',
+  donated: false,
+  donatedAmount: 0
+}
